92792ca14ed365a7f29311a4d18ca1d0f7485c14
Sam Yadav <samyadave@gmail.com>
2022-03-02 21:01:43 -0800

    lab work when we were both kinda tired

--- src/Graph.java
+++ src/Graph.java
@@ -28,7 +28,14 @@ public class Graph {
      * @param n the number of vertices in the graph
      */
     public Graph(int n) {
-
+        this.vertices = n;
+        this.edges = 0;
+        adj = new ArrayList<>(n);
+        color = new ArrayList<>(n);
+        distance = new ArrayList<>(n);
+        parent = new ArrayList<>(n);
+        discoverTime = new ArrayList<>(n);
+        finishTime = new ArrayList<>(n);
     }
 
     /*** Accessors ***/
@@ -39,7 +46,7 @@ public class Graph {
      * @return the number of edges
      */
     public int getNumEdges() {
-
+        return this.edges;
     }
 
     /**
@@ -48,16 +55,17 @@ public class Graph {
      * @return the number of vertices
      */
     public int getNumVertices() {
-
+        return this.vertices;
     }
 
     /**
      * returns whether the graph is empty (no edges)
+     * AND ALSO DON'T FORGET ABOUT THE VERTICES SMH
      * 
      * @return whether the graph is empty
      */
     public boolean isEmpty() {
-
+        return (edges == 0 && vertices == 0);
     }
 
     /**
@@ -70,7 +78,11 @@ public class Graph {
      *                                   v is out of bounds
      */
     public Integer getDistance(Integer v) throws IndexOutOfBoundsException {
-
+        if (v < 0 && v > this.vertices) {
+            throw new IndexOutOfBoundsException(
+                    "getDistance(): Vertex is either negative or is greater than number of vertices!");
+        }
+        return this.distance.get(v);
     }
 
     /**
@@ -83,7 +95,12 @@ public class Graph {
      *                                   v is out of bounds
      */
     public Integer getParent(Integer v) throws IndexOutOfBoundsException {
+        if (v > this.vertices || v < 0) {
+            throw new IndexOutOfBoundsException(
+                    "getParent(): Vertex is either negative or is greater than number of vertices!");
+        }
 
+        return this.parent.get(v) == null ? -1 : this.parent.get(v);
     }
 
     /**
@@ -96,7 +113,11 @@ public class Graph {
      *                                   v is out of bounds
      */
     public Character getColor(Integer v) throws IndexOutOfBoundsException {
-
+        if (v < 0 && v > vertices) {
+            throw new IndexOutOfBoundsException(
+                    "getColor(): Vertex is either negative or is greater than number of vertices!");
+        }
+        return this.color.get(v);
     }
 
     /**
@@ -109,7 +130,11 @@ public class Graph {
      *                                   v is out of bounds
      */
     public Integer getDiscoverTime(Integer v) throws IndexOutOfBoundsException {
-
+        if (v < 0 && v > vertices) {
+            throw new IndexOutOfBoundsException(
+                    "getDiscoverTime(): Vertex is either negative or is greater than number of vertices!");
+        }
+        return this.discoverTime.get(v);
     }
 
     /**
@@ -122,7 +147,11 @@ public class Graph {
      *                                   v is out of bounds
      */
     public Integer getFinishTime(Integer v) throws IndexOutOfBoundsException {
-
+        if (v < 0 && v > this.vertices) {
+            throw new IndexOutOfBoundsException(
+                    "getFinishTime(): Vertex is either negative or is greater than number of vertices!");
+        }
+        return this.finishTime.get(v);
     }
 
     /**
@@ -135,6 +164,11 @@ public class Graph {
      *                                   v is out of bounds
      */
     public LinkedList<Integer> getAdjacencyList(Integer v) throws IndexOutOfBoundsException {
+        if (v < 0 && v > vertices) {
+            throw new IndexOutOfBoundsException(
+                    "getAdjacencyList(): Vertex is either negative or is greater than number of vertices!");
+        }
+        return this.adj.get(v);
 
     }
 
@@ -149,7 +183,10 @@ public class Graph {
      *                          u or v is out of bounds
      */
     public void addDirectedEdge(Integer u, Integer v) throws IndexOutOfBoundsException {
-
+        if (v < 0 && v > vertices && (u < 0 && u > vertices)) {
+            throw new IndexOutOfBoundsException("addDirectedEdge(): Either u or v is out of bounds!"); // yes
+        }
+        this.adj.get(u).addLast(v); // ???
     }
 
     /**
@@ -162,7 +199,12 @@ public class Graph {
      *                                   u or v is out of bounds
      */
     public void addUndirectedEdge(Integer u, Integer v) throws IndexOutOfBoundsException {
+        if (v < 0 && v > vertices && (u < 0 && u > vertices)) {
+            throw new IndexOutOfBoundsException("addDirectedEdge(): Either u or v is out of bounds!"); // yes
+        }
 
+        this.adj.get(u).addLast(v); // ???
+        this.adj.get(v).addLast(u); // ???
     }
 
     /*** Additional Operations ***/
@@ -174,7 +216,12 @@ public class Graph {
      */
     @Override
     public String toString() {
+        String str = "";
+        for (int i = 1; i < this.vertices; i++) {
 
+        }
+
+        return str + "\n";
     }
 
     /**
@@ -187,15 +234,120 @@ public class Graph {
      */
 
     public void BFS(Integer source) throws IndexOutOfBoundsException {
+        if (source < 0 && source > vertices) {
+            throw new IndexOutOfBoundsException(
+                    "BFS(): Source is either negative or is greater than number of vertices!");
+        }
 
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        for (int x = 1; x <= this.vertices; x++) {
+            this.color.add(x, 'W');
+            this.distance.add(x, -1);
+            this.parent.add(x, null);
+        }
+
+        this.color.add(source, 'G');
+        this.distance.add(source, 0);
+        queue.addFirst(source);
+        while (!queue.isEmpty()) {
+            int x = queue.getFirst();
+            queue.removeFirst();
+            for (int i = 0; i < this.adj.get(x).getLength(); i++) {
+                if (this.color.get(this.adj.get(x).findIndex(i)).equals('W')) {
+                    this.color.set(this.adj.get(x).findIndex(i), 'G');
+                    this.parent.set(this.adj.get(x).findIndex(i), x);
+                    this.distance.set(this.adj.get(x).findIndex(i), this.distance.get(x) + 1);
+                    queue.addLast(this.adj.get(x).findIndex(i));
+                }
+            }
+            this.color.set(x, 'B');
+        }
     }
+    // for all x in V(G)
+
+    // color[x] = white
+
+    // distance[x] = -1
+
+    // parent[x] = Nil
+
+    // color[s] = grey
+
+    // distance[s] = 0
+
+    // Enqueue(Q,s)
+
+    // while(Q is not empty)
+
+    // x = front of Q
+
+    // Dequeue(Q,x)
+
+    // for all y in adj[x]
+
+    // if color[y] == white
+
+    // color[y] = grey
+
+    // distance[y] = distance[x] + 1
+
+    // parent[y] = x
+
+    // Enqueue(Q, y)
+
+    // color[x] = black
+    // }
 
     /**
      * Performs depth first search on this Graph
      * in order of vertex lists
      */
     public void DFS() {
-
+        /**
+         * FS(G)
+         * 
+         * for all x in V(G)
+         * 
+         * color[x] = W
+         * 
+         * parent[x] = NIL
+         * 
+         * discovery_time[x] = -1
+         * 
+         * finish_time[x] = -1
+         * 
+         * time = 0
+         * 
+         * for all x in V(G)
+         * 
+         * if color[x] == W
+         * 
+         * Visit(x)
+         * 
+         * 
+         * 
+         * Visit(x)
+         * 
+         * color[x] = G
+         * 
+         * discover[x] = ++time
+         * 
+         * for all y in adj[x]
+         * 
+         * if color[y] == W
+         * 
+         * parent[y] = x
+         * 
+         * Visit(y)
+         * 
+         * color[x] = B
+         * 
+         * finish[x] = ++time
+         * 
+         * 
+         * 
+         */
     }
 
     /**
